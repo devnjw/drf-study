5ff0b8b7e69729be4b9650d52d0c03583e9e9ee4
devnjw <devnjw@gmail.com>
2022-01-06 15:10:33 +0900

    permission test

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -26,6 +26,26 @@ from snippets.serializers import UserSerializer
 
 from rest_framework import permissions
 
+from rest_framework.reverse import reverse
+from rest_framework import renderers
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
+
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
@@ -111,7 +131,6 @@ class SnippetList(generics.ListCreateAPIView):
 #             serializer.save()
 #             return JsonResponse(serializer.data, status=201)
 #         return JsonResponse(serializer.errors, status=400)
-
 
 from snippets.permissions import IsOwnerOrReadOnly
 
